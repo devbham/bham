d9a16e1a0fe3a934362034b04d32f1cb7e91a0d1
unknown <tomson.ngassa@TomsonNgassaPC.fei.local>
2014-01-13 09:29:13 -0500

    Add updated groovy script for calling Grunt command

--- buildsrc/src/main/groovy/com/fei/systems/client/GruntTask.groovy
+++ buildsrc/src/main/groovy/com/fei/systems/client/GruntTask.groovy
@@ -20,53 +20,6 @@ class GruntTask extends Exec {
 	}
 }
 
-//
-//class GruntTask extends Exec {
-//	private String gruntExecutable = Os.isFamily(Os.FAMILY_WINDOWS) ? "grunt.cmd" : "grunt"
-//	def gruntArgs = ""
-//	def output = new ByteArrayOutputStream()
-//	def File outputFile = null
-//	
-//	public GruntTask() {
-//		super()
-//		this.setExecutable(gruntExecutable)
-//		// Don't fail immediately after executing the command, we must save the output to a file first...
-//		this.setIgnoreExitValue(true)
-//		// Capture output...
-//		this.setStandardOutput(output)
-//		this.setErrorOutput(output)
-//	}
-//	
-//	public void setGruntArgs(String gruntArgs) {
-//		this.args = "$gruntArgs".trim().split(" ") as List
-//		
-//		// Construct an output file name with gruntArgs...
-//		def reportsDir = new File(project.buildDir, "reports")
-//		this.outputFile = new File(reportsDir, gruntArgs +".txt")
-//		this.outputs.file outputFile
-//		
-//		// After executing command...
-//		this.doLast {
-//		
-//			// Save output to a file
-//			output.close()
-//			outputFile.parentFile.mkdirs()
-//			outputFile.text = output.toString()
-//			
-//			// Log errors (if any)
-//			def result = getExecResult()
-//		
-//			if (result.exitValue != 0) {
-//				logger.error(output.toString())
-//			}
-//			
-//			// Fail build if Grunt failed
-//			result.assertNormalExitValue()
-//		}
-//	}
-//
-//	
-//}
 	
 	
 	
